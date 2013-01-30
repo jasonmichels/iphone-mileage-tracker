2d7ff864ff59e90cad8778448872eead5a57be70
Jason <thebizztech@me.com>
2013-01-30 16:11:21 -0600

    - Initial commit

--- truckMileageTracker/trackerFirstViewController.h
+++ truckMileageTracker/trackerFirstViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface trackerFirstViewController : UIViewController
+<UIPickerViewDelegate, UIPickerViewDataSource>{
+    UIPickerView *statePicker;
+    NSArray *pickerData;
+    UILabel *stateChosen;
+}
+
+@property (nonatomic, retain) IBOutlet UIPickerView *statePicker;
+@property (nonatomic, retain) NSArray *pickerData;
+@property (nonatomic, retain) IBOutlet UILabel *stateChosen;
+- (IBAction)buttonPressed:(id)sender;
 
 @end
--- truckMileageTracker/trackerFirstViewController.m
+++ truckMileageTracker/trackerFirstViewController.m
@@ -13,9 +13,29 @@
 @end
 
 @implementation trackerFirstViewController
+@synthesize statePicker;
+@synthesize pickerData;
+@synthesize stateChosen;
+
+-(IBAction)buttonPressed:(id)sender{
+    NSInteger row = [statePicker selectedRowInComponent:0];
+    NSString *selected = [pickerData objectAtIndex:row];
+    
+    self.stateChosen.text = selected;
+    
+    NSString *title = [[NSString alloc] initWithFormat:@"You selecterd %@!", selected];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:@"Thank you for choosing." delegate:nil cancelButtonTitle:@"You're Welcome" otherButtonTitles:nil, nil];
+    
+    statePicker.hidden = true;
+    
+    [alert show];
+}
 
 - (void)viewDidLoad
 {
+    NSArray *array = [[NSArray  alloc] initWithObjects:@"MN", @"IA", @"NE", nil, nil];
+    self.pickerData = array;
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -24,6 +44,23 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return [pickerData count];
+}
+
+#pragma mark Picker Delegate Methods
+-(NSString *)pickerView:(UIPickerView *)pickerView
+            titleForRow:(NSInteger)row
+           forComponent:(NSInteger)component{
+    return [pickerData objectAtIndex:row];
 }
 
 @end
