99a6b539f5b3efd87a75aacd355db8343e4ef1a7
Jason Michels <thebizztech@me.com>
2013-06-07 11:35:11 -0500

    - Have mostly working restructure and redesign of code

--- truckMileageTracker/trackerFirstViewController.h
+++ truckMileageTracker/trackerFirstViewController.h
@@ -10,40 +10,39 @@
 
 #define kFilename @"mileage.plist"
 #define state @"state"
-#define mileOne @"mileOne"
-#define mileTwo @"mileTwo"
-#define mileThree @"mileThree"
-#define mileFour @"mileFour"
-#define mileFive @"mileFive"
-#define mileSix @"mileSix"
-#define mileSeven @"mileSeven"
 #define finalMileage @"finalMileage"
 #define defaultState @"Nebraska"
 #define zero @"0"
 
 @interface trackerFirstViewController : UIViewController
-<UIPickerViewDelegate, UIPickerViewDataSource>{
-    UIPickerView *statePicker;
-    NSArray *pickerData;
-    NSString *statePicked;
-    UIPickerView *mileagePicker;
+{
     NSArray *mileageData;
-    UILabel *stateLabel;
-    UIButton *selectButton;
     NSMutableArray *fileMileage;
+    
+    UITextField *stateField;
+    UITextField *mileageField;
+    UIButton *selectButton;
+    NSString *statePicked;
+    NSString *mileage;
+    NSArray *states;
 }
 
-@property (nonatomic, retain) IBOutlet UIPickerView *statePicker;
-@property (nonatomic, retain) NSArray *pickerData;
-@property (nonatomic, retain) NSString *statePicked;
+@property IBOutlet UITextField *stateField;
+@property IBOutlet UITextField *mileageField;
+@property IBOutlet UIButton *selectButton;
+@property NSString *statePicked;
+@property NSString *mileage;
+@property NSArray *states;
 
-@property (nonatomic, retain) IBOutlet UIPickerView *mileagePicker;
+
+
+
+
+
+
 @property (nonatomic, retain) NSArray *mileageData;
 //this should hold an array of probably dictionary
 @property (nonatomic, retain) NSMutableArray *fileMileage;
-
-@property (nonatomic, retain) IBOutlet UILabel *stateLabel;
-@property (nonatomic, retain) IBOutlet UIButton *selectButton;
 
 
 - (IBAction)buttonPressed:(id)sender;
--- truckMileageTracker/trackerFirstViewController.m
+++ truckMileageTracker/trackerFirstViewController.m
@@ -13,12 +13,9 @@
 @end
 
 @implementation trackerFirstViewController
-@synthesize statePicker;
-@synthesize mileagePicker;
+@synthesize stateField, mileageField, mileage, states;
 @synthesize mileageData;
-@synthesize pickerData;
 @synthesize statePicked;
-@synthesize stateLabel;
 @synthesize selectButton;
 @synthesize fileMileage;
 
@@ -30,180 +27,99 @@
 }
 
 -(IBAction)buttonPressed:(id)sender{
-    //This is for the state
-    NSInteger stateRow = [statePicker selectedRowInComponent:0];
-    self.statePicked = [pickerData objectAtIndex:stateRow];
     
-    if( statePicker.hidden == false){
-        //the state picker is shown right now so hide it
-        self.statePicker.hidden = true;
-        self.mileagePicker.hidden = false;
-        self.stateLabel.text = self.statePicked;
-        self.stateLabel.hidden = false;
-        [self.selectButton setTitle:@"Select Mileage" forState:UIControlStateNormal];
-        
-    }else{
-
-        //create a dictionary with an item for each number and state
-        NSMutableDictionary *newRow = [NSMutableDictionary new];
-        [newRow setObject: self.statePicked forKey: state];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:0]]
-                   forKey: mileOne];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:1]]
-                   forKey: mileTwo];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:2]]
-                   forKey: mileThree];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:3]]
-                   forKey: mileFour];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:4]]
-                   forKey: mileFive];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:5]]
-                   forKey: mileSix];
-        [newRow setObject: [mileageData objectAtIndex:[mileagePicker selectedRowInComponent:6]]
-                   forKey: mileSeven];
-        
-        //save the date information
-        NSDateComponents *components = [[NSCalendar currentCalendar] components:NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit fromDate:[NSDate date]];
-        [newRow setObject: [NSString stringWithFormat:@"%d", [components day]] forKey: @"day"];
-        [newRow setObject: [NSString stringWithFormat:@"%d", [components month]] forKey: @"month"];
-        [newRow setObject: [NSString stringWithFormat:@"%d", [components year]] forKey: @"year"];
-        
-        NSMutableString *finalMileageRow = [[NSString stringWithFormat:@"%@%@%@%@%@%@%@",
-                                                 [newRow objectForKey:mileOne],
-                                                 [newRow objectForKey:mileTwo],
-                                                 [newRow objectForKey:mileThree],
-                                                 [newRow objectForKey:mileFour],
-                                                 [newRow objectForKey:mileFive],
-                                                 [newRow objectForKey:mileSix],
-                                                 [newRow objectForKey:mileSeven]] mutableCopy];
-        
-        [newRow setObject:[NSNumber numberWithInt:[finalMileageRow intValue]] forKey:finalMileage];
-        
-        //add the dictionary to the array before saving to file
-        [self.fileMileage addObject:newRow];
-        [self.fileMileage writeToFile:[self dataFilePath] atomically:YES];
-        
-        NSMutableString *savedMessage = [[ NSString stringWithFormat:@"Your state of %@ and mileage of %@ have been saved. Select History to view quarterly summary.", self.statePicked, finalMileageRow] mutableCopy];
-
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"State & Mileage Saved" message:savedMessage delegate:nil cancelButtonTitle:@"Continue" otherButtonTitles:nil, nil];
-        
-        self.mileagePicker.hidden = true;
-        self.statePicker.hidden = false;
-        self.stateLabel.hidden = true;
-        [self.selectButton setTitle:@"Select State" forState:UIControlStateNormal];
-        [alert show];
+    // Get state and mileage from textfield
+    self.statePicked = [NSString stringWithFormat:@"%@",self.stateField.text];
+    NSLog(@"Original state picked, %@", self.stateField.text);
+    
+    self.mileage = [NSString stringWithFormat:@"%@",self.mileageField.text];
+    NSLog(@"Original mileage, %@", self.mileage);
+    
+    // @todo add validation for mileage and state
+    if (self.statePicked.length == 0) {
+        NSLog(@"State picked is empty");
+    }
+    
+    if (self.mileage.length == 0) {
+        NSLog(@"Mileage is empty");
+    }
+    
+    // convert abbreviation into long name
+    if (self.statePicked.length == 2) {
+        // Loop through states until find a match then set the state picked to the long version
+        for (NSUInteger i = 0; i < [self.states count]; i++) {
+            // Create new dictionary from row in array
+            NSDictionary *row = [self.states objectAtIndex:i];
+            
+            if ([self.statePicked.uppercaseString isEqualToString:[row objectForKey:@"abbreviation"]]) {
+                self.statePicked = [row objectForKey:@"name"];
+            }
+        }
         
     }
+    
+    
+    //create a dictionary with an item for each number and state
+    NSMutableDictionary *newRow = [NSMutableDictionary new];
+    [newRow setObject: self.statePicked forKey: state];
+    [newRow setObject:[NSNumber numberWithInt:[self.mileage intValue]] forKey:finalMileage];
+    
+    //save the date information
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit fromDate:[NSDate date]];
+    [newRow setObject: [NSString stringWithFormat:@"%d", [components day]] forKey: @"day"];
+    [newRow setObject: [NSString stringWithFormat:@"%d", [components month]] forKey: @"month"];
+    [newRow setObject: [NSString stringWithFormat:@"%d", [components year]] forKey: @"year"];
+    
+    //add the dictionary to the array before saving to file
+    [self.fileMileage addObject:newRow];
+    [self.fileMileage writeToFile:[self dataFilePath] atomically:YES];
+    
+    NSMutableString *savedMessage = [[ NSString stringWithFormat:@"Your state of %@ and mileage of %@ have been saved. Select History to view quarterly summary.", self.statePicked, self.mileage] mutableCopy];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"State & Mileage Saved" message:savedMessage delegate:nil cancelButtonTitle:@"Continue" otherButtonTitles:nil, nil];
+    
+    [alert show];
     
 }
 
 - (void)viewDidLoad
 {
+    [super viewDidLoad];
+    
     //lets load all the mileage from the file
     NSString *filePath = [self dataFilePath];
     
     if( [[NSFileManager defaultManager] fileExistsAtPath:filePath] ){
         fileMileage = [[NSMutableArray alloc] initWithContentsOfFile:filePath];
     }else{
-        // Since no file exists show the message how to use this app
+        // @todo Since no file exists show the message how to use this app
         fileMileage = [[NSMutableArray alloc] init];
     }
     
-    //get list of states to show in picker
-    NSString *statesPath = [[NSBundle mainBundle] pathForResource:
-                      @"states" ofType:@"plist"];
-    self.pickerData = [[NSArray alloc] initWithContentsOfFile:statesPath];
-    
     //get the count of fileMileages
     NSInteger fileMileageCount = [self.fileMileage count];
-    NSString *savedState;
-    NSArray *defaultMileageArray;
+    NSString *savedMileage;
     
     if(fileMileageCount > 0){
         //now get the last mileage
         NSDictionary *lastMileage = [ fileMileage objectAtIndex:(fileMileageCount - 1)];
         //this savedState will come from database previous choice
-        savedState = [lastMileage objectForKey:state];
-        
-        defaultMileageArray = [[NSArray alloc]
-                             initWithObjects:
-                                [lastMileage objectForKey:mileOne],
-                                [lastMileage objectForKey:mileTwo],
-                                [lastMileage objectForKey:mileThree],
-                                [lastMileage objectForKey:mileFour],
-                                [lastMileage objectForKey:mileFive],
-                                [lastMileage objectForKey:mileSix],
-                                [lastMileage objectForKey:mileSeven],
-                                nil];
+        savedMileage = [lastMileage objectForKey:finalMileage];
         
     }else{
-        savedState = defaultState;
-        //default array of numbers to show
-        defaultMileageArray = [[NSArray alloc]
-                                      initWithObjects:zero, zero, zero, zero, zero, zero, zero, nil];
+        savedMileage = @"0";
     }
     
-    //now find the state in the states array
-    NSInteger selectedState = [self.pickerData indexOfObject:savedState];
-    //set the state row based on previous selection
-    [self.statePicker selectRow:selectedState inComponent:0 animated:YES];
-    
-    //get array of last saved mileage
-    self.mileageData = [[NSArray alloc] initWithObjects:@"0", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", nil];
-    
-    for(int i = 0; i < 7; i++) {
-        //get the choice from the saved mileage array and get the intValue to set the component row
-        [self.mileagePicker selectRow:[[defaultMileageArray objectAtIndex:i] intValue] inComponent:i animated:YES];
-    }
-
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //get list of states to show in picker
+    NSString *statesPath = [[NSBundle mainBundle] pathForResource:
+                            @"states" ofType:@"plist"];
+    self.states = [[NSArray alloc] initWithContentsOfFile:statesPath];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark -
-#pragma mark Picker Data Source Methods
--(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-    
-    if(pickerView.tag ==1){
-        //This is the state picker        
-        return 1;
-    }else{
-        //this is the picker for the numbers
-        return 7;
-    }
-}
-
--(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
-    
-    if(pickerView.tag ==1){
-        //This is the state picker
-        return [pickerData count];
-    }else{
-        //this is the picker for the numbers
-        return [mileageData count];
-    }
-    
-}
-
-#pragma mark Picker Delegate Methods
--(NSString *)pickerView:(UIPickerView *)pickerView
-            titleForRow:(NSInteger)row
-           forComponent:(NSInteger)component{
-    
-    if(pickerView.tag ==1){
-        //This is the state picker
-        return [pickerData objectAtIndex:row];
-    }else{
-        //this is the picker for the numbers
-        return [mileageData objectAtIndex:row];
-    }
-    
 }
 
 @end
